25a134444f72d4f8e1fb8ba3e63710e589098186
PHANTOM <62259812+hadesphantom12@users.noreply.github.com>
2025-10-03 22:55:54 +0800

    Update hot.py

--- app/menus/hot.py
+++ app/menus/hot.py
@@ -82,7 +82,7 @@ def show_hot_menu2():
         print("===================🔥 Paket  Hot 2 🔥==================")
         print("=======================================================")
         
-        url = "https://me.mashu.lol/pg-hot2.json"
+        url = ""
         response = requests.get(url, timeout=30)
         if response.status_code != 200:
             print("Gagal mengambil data hot package.")
